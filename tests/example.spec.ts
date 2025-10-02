2f2b0285e783a194bec614c42ea89efae9bc0eb6
divinetragedy <nonliterarychannel@gmail.com>
2025-10-02 17:42:02 +0500

    Исправлен README

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-// Константы
 const URL = 'https://playwright.dev/';
 const EXPECTED_TITLE = 'Fast and reliable end-to-end testing for modern web apps | Playwright';
 
