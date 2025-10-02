ac399a5991db004fd9dba7a271509d2b0de8cfb9
divinetragedy <nonliterarychannel@gmail.com>
2025-10-02 15:31:12 +0500

    Изменен README и добавлены улучшения в код

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from '@playwright/test';
 
+// Константы
+const URL = 'https://playwright.dev/';
+const EXPECTED_TITLE = 'Fast and reliable end-to-end testing for modern web apps | Playwright';
+
 test('Проверка заголовка Playwright.dev', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(
-      'Fast and reliable end-to-end testing for modern web apps | Playwright'
-  );
+  await page.goto(URL);
+  await expect(page).toHaveTitle(EXPECTED_TITLE);
 });
